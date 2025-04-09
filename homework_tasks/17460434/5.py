ad02efcaa1fe3cf0a53b7a38dc92c4420be4242c
Hiron2305 <99662358+Hiron2305@users.noreply.github.com>
2025-04-09 17:38:36 +0300

    Create 5.py

---
+++ homework_tasks/17460434/5.py
@@ -0,0 +1,11 @@
+n = 0
+trigger = 0
+
+while trigger < 123:
+    to_bin = str(bin(n))[2:]
+    to_bin += str(sum(map(int, to_bin)) % 2)
+    to_bin += str(sum(map(int, to_bin)) % 2)
+
+    trigger = int(to_bin, 2)
+    n += 1
+    print(trigger)
