ef5577b958dce0f5173156c93a1d2c18882c9804
Hiron2305 <99662358+Hiron2305@users.noreply.github.com>
2025-04-10 14:14:43 +0300

    Update 2_1.py

--- homework_tasks/17703280/2_1.py
+++ homework_tasks/17703280/2_1.py
@@ -1,1 +1,10 @@
+#(x → (y → z)) ∧ (y → (z ≡ ¬ w))
+print("x y z w")
 
+for x in 0,1:
+    for y in 0, 1:
+        for z in 0, 1:
+            for w in 0, 1:
+                F = (x <= (y <= z)) and (y <= (z != w))
+                if F == 0:
+                    print(x, y, z, w)
