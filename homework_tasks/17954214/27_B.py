2d6a0967fd66e7bf508301d1a701231b9b528ff7
Hiron2305 <99662358+Hiron2305@users.noreply.github.com>
2025-04-11 15:34:06 +0300

    Create 27_B.py

---
+++ homework_tasks/17954214/27_B.py
@@ -0,0 +1,20 @@
+nums_a, nums_b, nums_c = [], [], []
+
+with open("27-B_8.txt") as file:
+    n = int(file.readline())
+    for i in range(n):
+        line = int(file.readline())
+        if line % 3 == 0:
+            nums_a.append(line)
+        elif line % 3 == 1:
+            nums_b.append(line)
+        else:
+            nums_c.append(line)
+
+nums_a.sort()
+nums_b.sort()
+nums_c.sort()
+
+sums = [nums_a[0] + nums_a[1] + nums_a[2], nums_b[0] + nums_b[1] + nums_b[2], nums_c[0] + nums_c[1] + nums_c[2], nums_a[0] + nums_b[0] + nums_c[0]]
+
+print(min(sums))
